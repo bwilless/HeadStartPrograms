e9487d94432eb98a7721b2879aaca57f390832b2
Brian Willess <brian_willess@cox.net>
2018-03-31 14:28:09 -0700

    Working beat box application

--- src/BeatBox.java
+++ src/BeatBox.java
@@ -1,3 +1,9 @@
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
 import javax.swing.*;
 
 import java.awt.BorderLayout;
@@ -10,15 +16,23 @@ import java.util.*;
 public class BeatBox {
 	
 	private JFrame frame;
-	private DrumType drums[];
-	private Font font;
-			
+	private ArrayList<DrumType> drums;
+	Sequencer sequencer;
+	Sequence sequence;
+	Track track;
+	
+	// Create each drumType
+	String[] drumTypeList = {"  Bass Drum", "  Closed Hi-Hat", "  Open Hi-Hat", 
+			"  Acoustic Snare", "  Crash Cymbal", "  Hand Clap", "  High Tom", 
+			"  Hi Bongo", "  Maracas", "  Whistle", "  Low Conga", "  Cowbell", 
+			"  Visraslap", "  Low-mid Tom", "  High Agogo", "  Open Hi Conga"};
+
+	int[] instrumentKey = {32,42,46,38,49,39,50,60,70,72,64,56,58,47,67,63};
+	
 	public static void main (String[] args) {
 	
 		BeatBox gui = new BeatBox();
 		gui.setupGui();
-		
-		
 	}
 	
 	private void setupGui() {
@@ -31,7 +45,6 @@ public class BeatBox {
 		JButton tempoUpButton;
 		JButton tempoDownButton;
 		JPanel checkBoxPanel[];
-		
 		
 		// Create the 4 buttons we need and a JPannel to hold them all
 		startButton = new JButton("Start");
@@ -55,17 +68,8 @@ public class BeatBox {
 		buttonPanel.add(tempoDownButton);
 		frame.getContentPane().add(BorderLayout.EAST, buttonPanel);
 
-		// define a font for the JLabel ojbects
-		font = new Font("Courier", Font.BOLD,15);
-		
 		// drums will hold each of the 16 drums.  
-		drums = new DrumType[16];
-		
-		// Create each drumType
-		String[] drumTypeList = {"  Bass Drum", "  Closed Hi-Hat", "  Open Hi-Hat", 
-				"  Acoustic Snare", "  Crash Cymbal", "  Hand Clap", "  High Tom", 
-				"  Hi Bongo", "  Maracas", "  Whistle", "  Low Conga", "  Cowbell", 
-				"  Visraslap", "  Low-mid Tom", "  High Agogo", "  Open Hi Conga"};
+		drums = new ArrayList<DrumType>();
 		
 		// instrumentTypePanel will hold all the labels for each drum type
 		JPanel instrumentTypePanel = new JPanel();
@@ -88,10 +92,10 @@ public class BeatBox {
 		for(int i = 0; i < 16; i++) {
 
 			// Create each drum list object.  Pass in a drum name String. 
-			drums[i] = new DrumType(drumTypeList[i]);
+			drums.add(new DrumType(drumTypeList[i]));
 						
 			// Add the drum.rowLabel to the instrumentTypePanel
-			instrumentTypePanel.add(drums[i].rowLabel);
+			instrumentTypePanel.add(drums.get(i).rowLabel);
 
 			// create a new checkBoxPanel.
 			checkBoxPanel[i] = new JPanel();
@@ -100,7 +104,7 @@ public class BeatBox {
 			// Add the checkBox List for the current drum type
 			for(int j = 0; j < 16; j++) {
 				
-				checkBoxPanel[i].add(drums[i].checkBoxList[j]);
+				checkBoxPanel[i].add(drums.get(i).checkBoxList.get(j));
 			}
 			instrumentCheckBoxPanel.add(checkBoxPanel[i]);
 		}
@@ -111,32 +115,85 @@ public class BeatBox {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(650, 400);
 		frame.setVisible(true);
+		
+		setUpMidi();
 	}
+	
+	public void buildTrackAndStart() {
+		int[] trackList = null;
+		
+		sequence.deleteTrack(track);
+		track = sequence.createTrack();
+		
+		for(int i = 0; i < 16; i++) {
+			trackList = new int[16];
+			
+			int key = instrumentKey[i];
+			
+			for(int j = 0; j < 16; j++) {
+				
+				JCheckBox jc = drums.get(i).checkBoxList.get(j);
+				
+				if(jc.isSelected()) {
+					trackList[j] = key;
+				} else {
+					trackList[j] = 0;
+				}
+			}
+			
+			makeTracks(trackList);
+			track.add(makeEvent(176,1,127,0,16));
+			
+		}
+		
+		track.add(makeEvent(192,9,1,0,15));
+		
+		try {
+			sequencer.setSequence(sequence);
+			sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+			sequencer.start();
+			sequencer.setTempoInBPM(120);
+		} catch (Exception e) {e.printStackTrace();}
+		
+	}
+	
 	
 	class DrumType {
 		
 		String drum;
-		JCheckBox [] checkBoxList = new JCheckBox[16];  
+		ArrayList<JCheckBox> checkBoxList = new ArrayList<JCheckBox>();
 		JLabel rowLabel;
-
-	DrumType (String drumName){
-		drum = drumName;
-		rowLabel = new JLabel(drumName);
-		rowLabel.setFont(font);
-		for(int i = 0; i < 16; i++) {
-			checkBoxList[i] = new JCheckBox();
-			checkBoxList[i].setSelected(false);
-			
+		// define a font for the JLabel ojbects
+		Font font = new Font("Courier", Font.BOLD,15);
+		
+		DrumType (String drumName){
+			drum = drumName;
+			rowLabel = new JLabel(drumName);
+			rowLabel.setFont(font);
+			for(int i = 0; i < 16; i++) {
+				checkBoxList.add(new JCheckBox());
+				checkBoxList.get(i).setSelected(false);
+			}
 		}
-	}
 		
 	}
 
+	public void setUpMidi() {
+		try {
+			sequencer = MidiSystem.getSequencer();
+			sequencer.open();
+			sequence = new Sequence(Sequence.PPQ, 4);
+			track = sequence.createTrack();
+			sequencer.setTempoInBPM(120);
+		} catch (Exception e) {e.printStackTrace();}
+	}
+		
 	class StartButton implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("User hit the Start Button");
+//			System.out.println("User hit the Start Button");
+			buildTrackAndStart();
 		}
 	}
 
@@ -144,7 +201,8 @@ public class BeatBox {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("User hit the Stop Button");
+//			System.out.println("User hit the Stop Button");
+			sequencer.stop();
 		}
 	}
 
@@ -152,7 +210,9 @@ public class BeatBox {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("User hit the Tempo Up Button");
+//			System.out.println("User hit the Tempo Up Button");
+			float tempoFactor = sequencer.getTempoFactor();
+			sequencer.setTempoFactor((float) (tempoFactor * 1.03));
 		}
 	}
 
@@ -161,7 +221,36 @@ public class BeatBox {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("User hit the Tempo Down Button");
+//			System.out.println("User hit the Tempo Down Button");
+			float tempoFactor = sequencer.getTempoFactor();
+			sequencer.setTempoFactor((float) (tempoFactor * .97));
+
 		}
 	}
+	
+	public void makeTracks(int[] list) {
+		for (int i = 0; i < 16; i++) {
+			int key = list[i];
+			
+			if(key != 0) {
+				track.add(makeEvent(144,9,key,100, i));
+				track.add(makeEvent(128,9,key,100, i+1));
+			}
+		}
+	}
+
+	public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+		
+		MidiEvent event = null;
+		
+		try {
+			ShortMessage a = new ShortMessage();
+			a.setMessage(comd, chan, one, two);
+			event = new MidiEvent(a, tick);
+		}catch (Exception e ) {e.printStackTrace();}
+		
+		return event;
+		
+	}
+	
 }
